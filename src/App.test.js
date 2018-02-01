f90a05cb736b98aefbdad5b5bf66fc5b1083e951
Vytenis <projektas@gmail.com>
2018-02-02 00:44:17 +0200

    hmz

---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+// @flow
+import * as React from 'react';
+
+import { shallow } from 'enzyme';
+
+import App from './App';
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
+
+it('renders welcome message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = 'Welcome to React';
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
+it('matches a snapshot', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
+});
---
+++ src/setupTests.js
@@ -0,0 +1,15 @@
+// @flow
+import { configure } from 'enzyme';
+import 'jest-enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+
+configure({ adapter: new Adapter() });
+
+const localStorageMock = {
+  getItem: jest.fn(),
+
+  setItem: jest.fn(),
+  clear: jest.fn(),
+};
+global.localStorage = localStorageMock;
