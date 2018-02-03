3fea090a8296c55fa94b7de3bbe7697dbd6c1731
Vytenis <projektas@gmail.com>
2018-02-03 15:57:57 +0200

    remove default export

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component<{}> {
+export class App extends React.Component<{}> {
   render() {
     return (
       <div className="App">
@@ -19,5 +19,3 @@ class App extends React.Component<{}> {
     );
   }
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 
-import App from './App';
+import { App } from './App';
 
 it('renders without crashing', () => {
   shallow(<App />);
